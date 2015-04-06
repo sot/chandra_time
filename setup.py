acca86aebc549d82b620d4d8aee6d9844f2dd3d5
Tom Aldcroft <taldcroft@gmail.com>
2015-04-06 08:32:38 -0400

    Fix setup.py to work with latest setuptools + cleanup of comments

--- setup.py
+++ setup.py
@@ -1,9 +1,7 @@
-# from distutils.core import setup, Extension
 from setuptools import setup, Extension
 
 import os
 from Cython.Build import cythonize
-from Cython.Build.Dependencies import create_extension_list
 
 if (os.name == "nt") :
     compile_args = ['/EHs','/D_CRT_SECURE_NO_DEPRECATE']
@@ -11,18 +9,8 @@ else:
     compile_args = ['-Wno-switch-enum', '-Wno-switch', '-Wno-switch-default',
                     '-Wno-deprecated', '-Wno-parentheses']
 
-# (gcc_stdin, gcc_stdouterr) = os.popen4(['gcc','-v', '--help'])
-# gcc_opt = gcc_stdouterr.read()
-# if gcc_opt.find('fstack-protector') != -1:
-#    compile_args.append('-fno-stack-protector')
-
-# machine = Popen(['uname','--machine'], stdout=PIPE).communicate()[0]
-# if machine.startswith('x86_64'):
-
-ext_modules = create_extension_list(["Chandra/*.pyx"])
-
-for module in ext_modules:
-    module.extra_compile_args = compile_args
+extensions = [Extension("*", ["Chandra/_axTime3.pyx"],
+                        extra_compile_args=compile_args)]
 
 setup(name='Chandra.Time',
       author='Tom Aldcroft',
@@ -34,6 +22,6 @@ setup(name='Chandra.Time',
       test_suite="Chandra.test_Time",
       namespace_packages=['Chandra'],
       packages=['Chandra'],
-      package_dir={'Chandra' : 'Chandra'},
-      ext_modules=cythonize(ext_modules),
+      package_dir={'Chandra': 'Chandra'},
+      ext_modules=cythonize(extensions),
       )
